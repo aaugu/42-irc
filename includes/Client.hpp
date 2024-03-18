c2f245ed22306ebc2af3d6f2b5a8eceb0cc623bd
vogtl <109412587+vogtl@users.noreply.github.com>
2024-03-18 12:58:04 +0100

    Revert "Aaugu channel"

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -3,85 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   Client.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
+/*   By: lvogt <lvogt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 11:37:30 by aaugu             #+#    #+#             */
-/*   Updated: 2024/03/18 12:02:57 by aaugu            ###   ########.fr       */
+/*   Updated: 2024/03/15 10:42:28 by lvogt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
 
-# include <string>
-# include <vector>
-# include <sstream>
-# include <poll.h>
-# include <errno.h>
 # include <string.h>
+# include <vector>
 
-# include "../includes/messages.hpp"
+# include "Server.hpp"
 
 struct s_message {
-    std::string fullStr;
-    std::string command;
+    std::string _fullStr;
+    std::string _command;
     std::vector<std::string> _paramsSplit;
     std::string _params;
 };
 
 class Server;
-class Channel;
 
 class Client
 {
     private:
 		int			_sockfd;
 		std::string	_nickname;
-		std::string	_address;
 		s_message	_message;
 		bool		_passwordReceved;
 		bool		_passwordChecked;
 		bool		_welcomSended;
-		Channel*	_currentChannel;
 
-		void		command_pass(Server &server, std::vector<pollfd>::iterator pollfd);
-		void		check_if_pass(Server &server, std::vector<pollfd>::iterator pollfd);
-
-		std::vector<std::string>	split(std::string value);
-		void 						splitMessage(std::string buff);
-		bool						checkUseNickname(Server *s, std::string &nickname);
+		// Client(void);
 
     public:
    		// Constructor and destructor
-		Client(int sockfd, std::string address);
+		Client(int sockfd);
 		~Client(void);
 
 		// Class function
 		void setData(Server *s, std::string &buffer);
 
-		// Gestion Input + Parsing + Execution
-        void		parseMessage(std::string buff);
-		void		exeCommand(Server* server, std::vector<pollfd>::iterator pollfd);
-		void		saveMessage(std::string buff);
-
 		// Accessors
 		int			getFd(void);
 		std::string getNickname(void);
-		std::string	getAddress(void);
+		// void		setFd(int value);
 		void		setNickname(std::string value);
-		void		setCurrentChannel(Channel* currentChannel);
 
-		// Send Message
-		template < typename T >
-		void	sendMessage(T message)
-		{
-			std::stringstream	ss;
-			ss << message << std::endl;
-			std::string			msg = ss.str();
+		// Gestion Input + Parsing + Execution
+		void 		splitMessage(std::string buff);
+        void		parseMessage(std::string buff);
+		void		exeCommand(Server &server, std::vector<pollfd>::iterator pollfd);
+		void		saveMessage(std::string buff);
+		void		send_to(std::string text) const;
+		
 
-			if (send(_sockfd, msg.c_str(), msg.size(), 0) < 0)
-				printErrMessage(errMessage("Could not send message to", _sockfd, strerror(errno)));
-		}
+		// command
+
+		void		command_pass(Server &server, std::vector<pollfd>::iterator pollfd);
+		void		check_if_pass(Server &server, std::vector<pollfd>::iterator pollfd);
 };
 
 #endif
--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Client.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
+/*   By: lvogt <lvogt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 11:43:23 by aaugu             #+#    #+#             */
-/*   Updated: 2024/03/18 12:04:10 by aaugu            ###   ########.fr       */
+/*   Updated: 2024/03/15 11:01:58 by lvogt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,44 @@
 #include "../includes/Client.hpp"
 #include "../includes/messages.hpp"
 #include "../includes/SendMessages.hpp"
-#include "../includes/CommandExec.hpp"
+
+std::vector<std::string> split(std::string value) {
+    std::istringstream iss(value);
+    std::vector<std::string> mots;
+    std::string mot;
+
+    while (iss >> mot)
+        mots.push_back(mot);
+    return mots;
+}
+
+bool checkUseNickname(Server *s, std::string &nickname) {
+    std::vector<std::string> nick = s->getNicknameList();
+    std::vector<std::string>::iterator it;
+
+    for (it = nick.begin(); it != nick.end(); ++it) {
+        if (*it == nickname)
+            return true;
+    }
+    return false;
+}
 
 /* ************************************************************************** */
 /*                          CONSTRUCTORS & DESTRUCTOR                         */
 /* ************************************************************************** */
 
-Client::Client(int sockfd, std::string address) :
-                _sockfd(sockfd),
-                _address(address),
-                _passwordReceved(false),
-                _passwordChecked(false),
-                _welcomSended(false) {}
+// Client::Client(void) {}
 
-Client::~Client(void) {}
+Client::Client(int sockfd) : _sockfd(sockfd) {
+    std::cout << "coucou" << std::endl;
+    _passwordReceved = false;
+    _passwordChecked = false;
+    _welcomSended = false;
+}
+
+Client::~Client(void) {
+    std::cout << "bye bye" << std::endl;
+}
 
 /* ************************************************************************** */
 /*                           PUBLIC MEMBER FUNCTION                           */
@@ -52,73 +76,11 @@ void Client::setData(Server *s, std::string &buffer) {
         if (dupe) {
             std::cout << "change nickname" << std::endl;
             std::string nickChangeMessage = ": NICK " + info[1];
-            sendMessage(nickChangeMessage);
-            sendMessage("Nickname changed to " + info[1]);
+            sendMessage(nickChangeMessage, _sockfd);
+            sendMessage("Nickname changed to " + info[1], _sockfd);
         }
         _nickname = info[1];
     }
-}
-
-void Client::saveMessage(std::string buff) {
-    _message.fullStr = _message.fullStr + buff;
-    std::cout << "_message.fullStr \"" << _message.fullStr << "\"" << std::endl;
-}
-
-void Client::exeCommand(Server* server, std::vector<pollfd>::iterator pollfd)
-{
-    CommandExec exec(server, this, &_message);
-
-    std::string type[] = {"PASS", "NICK", "USER", "JOIN"}; //ajout d'autre commande
-    int count = 0;
-    size_t arraySize = sizeof(type) / sizeof(type[0]);
-    for (int i = 0; i < (int)arraySize; i++){
-        if (_message.command.compare(type[i]) != 0)
-            count++;
-        else
-            break;
-    }
-
-    switch (count) {
-        case 0:
-            std::cout << "TO DO PASS OF \"" << _message._params << "\"" << std::endl;
-            command_pass(*server, pollfd);
-            break;
-        case 1:
-            // command_nick();
-            std::cout << "TO DO NICK OF \"" << _message._params << "\"" << std::endl;
-            check_if_pass(*server, pollfd);
-            _nickname = _message._params;
-            if (_passwordReceved == true && _passwordChecked == true && _welcomSended == false){
-                sendMessage(RPL_WELCOME(_nickname, "_user", "_hostName"));
-                _welcomSended = true;
-            }
-            break;
-        case 2:
-            // command_user();
-            check_if_pass(*server, pollfd);
-            std::cout << "TO DO USER OF \"" << _message._params << "\"" << std::endl;
-            break;
-        case 3:
-            // command_join();
-            std::cout << "TO DO JOIN OF \"" << _message._params << "\"" << std::endl;
-            if(_message._params.compare(":") == 0 && _passwordReceved == false){
-                sendMessage(ERR_NOTREGISTERED(_nickname));
-                break;
-            }
-            check_if_pass(*server, pollfd);
-            exec.join();
-            break;
-        // case 4:
-        //     ...
-    }
-}
-
-void Client::parseMessage(std::string buff) {
-    _message.fullStr = _message.fullStr + buff;
-    std::cout << "Client " << _sockfd << ": " << _message.fullStr << std::endl;;
-    splitMessage(_message.fullStr);
-    _message.fullStr.erase();
-    std::cout << "_message.fullStr aftersplit\"" << _message.fullStr << "\"" << std::endl;
 }
 
 /* ************************************************************************** */
@@ -133,50 +95,12 @@ std::string Client::getNickname() {
     return _nickname;
 }
 
-std::string	Client::getAddress(void) {
-    return ( _address );
-}
+// void Client::setFd(int value) {
+//     _sockfd = value;
+// }
 
 void Client::setNickname(std::string value) {
     _nickname = value;
-}
-
-void    Client::setCurrentChannel(Channel* currentChannel) {
-    _currentChannel = currentChannel;
-}
-
-/* ************************************************************************** */
-/*                             PRIVATE FUNCTIONS                              */
-/* ************************************************************************** */
-
-void Client::command_pass(Server &server, std::vector<pollfd>::iterator pollfd) {
-    _passwordReceved = true;
-    if (_message._params.compare(server.get_password()) == 0) {
-        _passwordChecked = true;
-    }
-    check_if_pass(server, pollfd);
-}
-
-void Client::check_if_pass(Server &server, std::vector<pollfd>::iterator pollfd) {
-    if (_passwordReceved == false) {
-        sendMessage(ERR_PASSWDMISS);
-        server.disconnectClient(pollfd);
-    }
-    else if (_passwordChecked == false) {
-        sendMessage(ERR_PASSWDMISMATCH);
-        server.disconnectClient(pollfd);
-    }
-
-}
-
-std::vector<std::string> Client::split(std::string value) {
-    std::istringstream iss(value);
-    std::vector<std::string> mots;
-    std::string mot;
-
-    while (iss >> mot)
-        mots.push_back(mot);
-    return mots;
 }
 
 void Client::splitMessage(std::string buff) {
@@ -187,7 +111,7 @@ void Client::splitMessage(std::string buff) {
     _message._params.clear();
     while (ss >> word) {
         if (count == 0)
-            _message.command = word;
+            _message._command = word;
         else if (count == 1)
         {
             _message._paramsSplit.push_back(word);
@@ -202,15 +126,85 @@ void Client::splitMessage(std::string buff) {
     }
 }
 
-bool Client::checkUseNickname(Server *s, std::string &nickname) {
-    std::vector<std::string> nick = s->getNicknameList();
-    std::vector<std::string>::iterator it;
+void	Client::send_to(std::string text) const {
+	send(_sockfd, text.c_str(), text.length(), 0);
+}
 
-    for (it = nick.begin(); it != nick.end(); ++it) {
-        if (*it == nickname)
-            return true;
+void Client::saveMessage(std::string buff) {
+    _message._fullStr = _message._fullStr + buff;
+    std::cout << "_message._fullStr \"" << _message._fullStr << "\"" << std::endl;
+}
+
+void Client::exeCommand(Server &server, std::vector<pollfd>::iterator pollfd) {
+    std::string type[] = {"PASS", "NICK", "USER", "JOIN"}; //ajout d'autre commande 
+    int count = 0;
+    size_t arraySize = sizeof(type) / sizeof(type[0]);
+    for (int i = 0; i < (int)arraySize; i++){
+        if (_message._command.compare(type[i]) != 0)
+            count++;
+        else
+            break;
     }
-    return false;
+    switch (count) {
+        case 0:
+            std::cout << "TO DO PASS OF \"" << _message._params << "\"" << std::endl;
+            command_pass(server, pollfd);
+            break;
+        case 1:
+            // command_nick();
+            std::cout << "TO DO NICK OF \"" << _message._params << "\"" << std::endl;
+            check_if_pass(server, pollfd);
+            _nickname = _message._params;
+            if (_passwordReceved == true && _passwordChecked == true && _welcomSended == false){
+                send_to(RPL_WELCOME(_nickname, "_user", "_hostName"));
+                _welcomSended = true;
+            }
+            break;
+        case 2:
+            // command_user();
+            check_if_pass(server, pollfd);
+            std::cout << "TO DO USER OF \"" << _message._params << "\"" << std::endl;
+            break;
+        case 3:
+            // command_join();
+            std::cout << "TO DO JOIN OF \"" << _message._params << "\"" << std::endl;
+            if(_message._params.compare(":") == 0 && _passwordReceved == false){
+                send_to(ERR_NOTREGISTERED(_nickname));
+                break;
+            }
+            check_if_pass(server, pollfd);
+            break;
+        // case 4:
+        //     ...
+    }
+}
+
+void Client::parseMessage(std::string buff) {
+    _message._fullStr = _message._fullStr + buff;
+    std::cout << "Client " << _sockfd << ": " << _message._fullStr << std::endl;;
+    splitMessage(_message._fullStr);
+    _message._fullStr.erase();
+    std::cout << "_message._fullStr aftersplit\"" << _message._fullStr << "\"" << std::endl;
+}
+
+void Client::command_pass(Server &server, std::vector<pollfd>::iterator pollfd) {
+    _passwordReceved = true;
+    if (_message._params.compare(server.get_password()) == 0) {
+        _passwordChecked = true;
+    }
+    check_if_pass(server, pollfd);
+}
+
+void Client::check_if_pass(Server &server, std::vector<pollfd>::iterator pollfd) {
+    if (_passwordReceved == false) {
+        send_to(ERR_PASSWDMISS);
+        server.disconnectClient(pollfd);
+    }
+    else if (_passwordChecked == false) {
+        send_to(ERR_PASSWDMISMATCH);
+        server.disconnectClient(pollfd);
+    }
+    
 }
 
 // /* ************************************************************************** */
